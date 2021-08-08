b7e1d1b9cfce46e85f117d555b506ceecfca796c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-08-08 20:50:43 +0500

    the algorithm has been tested. Its complexity is O(n)

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -1,14 +1,17 @@
 let twoSum = function(nums, target) {
-  let result = []
-  let size = nums.length
-  
-  for (i=0; i < size; i++) { //внешний цикл
-      for (k=i+1; k <= size; k++) { //внутренний цикл 
-        if (nums[i] + nums[k] === target)  {return result.concat([i],[k])} 
-      }
-  } 
-};
+  let result = []  
 
-const nums = [2,7,11,15]
-const target = 26
-console.log(twoSum(nums,target))
+//Определение набора чисел, которые могут являться слагаемыми для target
+let i = 0
+
+while(i < nums.length) {
+  let oneTerms = nums[i]
+  let twoTerms = target - oneTerms
+  if (nums.includes(twoTerms) && nums.lastIndexOf(twoTerms) !== i) {
+    result.unshift(nums.indexOf(nums[i]), nums.lastIndexOf(target - nums[i]))
+    break
+  }
+  i++
+}
+return result
+};
